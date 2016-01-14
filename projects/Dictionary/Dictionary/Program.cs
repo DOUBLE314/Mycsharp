6ab5e68b0c48803396108dcf06b8771816d43b91
Valerych <valerij.zhigunov@gmail.com>
2016-01-14 18:18:43 +0300

    Abstract class Dictionary

---
+++ projects/Dictionary/Dictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Dictionary
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+      static void Main()
+        {
+                Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("Троу [Throw]", "Сроу [θrəʊ]" );
+            dic.Add("Парсе [Parse]", "Парс [pɑːrs]");
+            dic.Add("Матч [Math]", "Мэс [mæθ]" );
+            dic.Add("Джет [Get]", "Гет [ɡet]" );
+            dic.Add("Трэй [Try]", "Трай [traɪ]";
+            dic.Add("Анчекид [Unchecked]", "Анчект [ʌnˈtʃekt]");
+            dic.Add("Имплой [Employee]", "Имплойи [ɪmˈplɔɪiː]");
+            
+            foreach (KeyValuePair<string, string> kvp in dic)
+            {
+                MessageBox.Show(string.Format("Правильное название {0} это {1}", kvp.Key, kvp.Value));
+            } 
+
+        }
+    }
+}
---
+++ projects/Dictionary/Dictionary/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Dictionary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dictionary")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2ca16d02-2dca-4e9c-9bdc-4f355cae855c")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
