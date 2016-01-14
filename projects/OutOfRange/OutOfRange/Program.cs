6c65a9d55eeecaa5069e7c6d4d96d02bde37247e
Valerych <valerij.zhigunov@gmail.com>
2016-01-14 18:20:53 +0300

    Out of range

---
+++ projects/OutOfRange/OutOfRange/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutOfRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int percent = 0;
+            Console.WriteLine("Введите количество процентов:");
+            percent = int.Parse(Console.ReadLine());
+                  try
+            {
+                if ((percent < 0) || (percent > 100))
+
+                    throw new ArgumentOutOfRangeException();
+                Console.WriteLine(percent);
+
+                  }
+            catch (ArgumentOutOfRangeException)
+            {
+
+                Console.WriteLine("Exception: Выход за границу диапазона");
+                
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
