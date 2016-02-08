29d1ea15fe53d6a6224e2fad76a2227ff5ac4660
Valerych <valerij.zhigunov@gmail.com>
2016-02-08 13:48:11 +0300

    Простое ветвление

---
+++ projects/Hours/Hours/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 5;
+            if (hour >= 0 && hour < 24)
+                Console.WriteLine("Переменная hour > 0 и меньше 24");
+            else
+                hour = 0;
+            Console.ReadLine();
+        }
+    }
+}
