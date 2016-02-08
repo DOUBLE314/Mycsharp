82e7db3c71e2dc82d32094a256e63e86085e105a
Valerych <valerij.zhigunov@gmail.com>
2016-02-08 13:34:03 +0300

    Обработка исключений с помощью операторов С# try-catch-finally! В блоке try идет получение ресурсов, т.е. что мы обрабатываем, в блоке catch выполняется обработка исключительных ситуаций, а в блоке finally происходит высвобождение ресурсов!

---
+++ projects/finally/finally/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    Console.WriteLine("Введите два числа");
+                    int a = int.Parse(Console.ReadLine());
+                    int b = int.Parse(Console.ReadLine());
+                    Console.WriteLine(a + "/" + b + "=" + a / b);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Нужно ввести число!");
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Делить на нуль нельзя!");
+                }
+                catch
+                {
+                    Console.WriteLine("Какая-то ошибка");
+                }
+                finally
+                {
+                    Console.WriteLine("после try-блока");
+                }
+            }
+
+        }
+    }
+}
